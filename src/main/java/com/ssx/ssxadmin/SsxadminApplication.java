4a4e7d8f78d51587d88b564410d642e4573e5eb6
zyj <454278709@qq.com>
2018-07-11 00:03:16 +0800

    initial version

---
+++ src/main/java/com/ssx/ssxadmin/SsxadminApplication.java
@@ -0,0 +1,13 @@
+package com.ssx.ssxadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsxadminApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SsxadminApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ssx/ssxadmin/SsxadminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ssx.ssxadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SsxadminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
